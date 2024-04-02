0986cfe6060b7c7154dc4b21551dc2c3c4e30a3e
Noelia Baños <102227812+Nissolia@users.noreply.github.com>
2024-04-02 13:44:08 +0200

    Delete 02_boletin_iterativas directory

--- 02_boletin_iterativas/03_boletin_extra/ej8.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-// Escribir un conjunto de instrucciones que permita mostrar un día de la semana dado un
-// número entero.
-int main() {
-    
-    return 0;
-}
