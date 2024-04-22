8e159b5ee84183b5640ba882a9d43c45f33c09f8
Noelia Baños <102227812+Nissolia@users.noreply.github.com>
2024-04-22 13:01:26 +0200

    Add files via upload

---
+++ JAVA/Interfaces_ej01/Socios02.java
@@ -0,0 +1,84 @@
+package Interfaces_ej01;
+
+public class Socios02 implements Comparable<Socios02> {
+	/* Atributos */
+	int id_socio;
+	String nombre;
+	double cuenta;
+
+	/* Constructores */
+	public Socios02(int id_socio, String nombre, double cuenta) {
+		super();
+		this.id_socio = id_socio;
+		this.nombre = nombre;
+		this.cuenta = cuenta;
+	}
+
+	/* Getter y setters */
+	public int getId_socio() {
+		return id_socio;
+	}
+
+	public void setId_socio(int id_socio) {
+		this.id_socio = id_socio;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(double cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	/* Métodos */
+	public static Socios02[] generarSocios(int cantidad) {
+		Socios02[] socios = new Socios02[cantidad];
+		int rand;
+		for (int i = 0; i < cantidad; i++) {
+			rand = (int) (Math.random() * 100);
+			int id_socio = i + 1;
+			String nombre = generarNombreAleatorio();
+			double cuenta = rand;
+			socios[i] = new Socios02(id_socio, nombre, cuenta);
+		}
+		return socios;
+	}
+
+	@Override
+	public int compareTo(Socios02 o) {
+		return Double.compare(this.cuenta, o.getCuenta());
+	}
+
+	// Método para generar un nombre aleatorio
+	private static String generarNombreAleatorio() {
+		String[] nombres = { "Juan", "María", "Pedro", "Ana", "Luis", "Sofía", "Carlos", "Laura", "Pablo", "Elena" };
+		int rand = (int) (Math.random() * 10);
+
+		return nombres[rand];
+	}
+	
+	public static void OrdenarSocios(Socios02[] socios) {
+	    int n = socios.length;
+	    for (int i = 0; i < n - 1; i++) {
+	        for (int j = 0; j < n - i - 1; j++) {
+	            if (socios[j].getCuenta() > socios[j + 1].getCuenta()) {
+	                // Intercambiar socios[j] y socios[j+1]
+	                Socios02 temp = socios[j];
+	                socios[j] = socios[j + 1];
+	                socios[j + 1] = temp;
+	            }
+	        }
+	    }
+	}
+
+
+}
--- JAVA/Interfaces_ej01/SociosMain.java
+++ JAVA/Interfaces_ej01/SociosMain.java
@@ -3,7 +3,32 @@ package Interfaces_ej01;
 public class SociosMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * main creacion de los socios, mostrarlos, metodos para ordenarlo, como le
+		 * pasamos el array se ordena por el propio array y luego lo mostramos de nuevo
+		 */
+
+		// Generar array de socios
+		Socios02[] socios = Socios02.generarSocios(5);
+
+		// Mostrar socios antes de ordenar
+		System.out.println("\tSocios antes de ordenar:");
+		imprimirSocios(socios);
+
+		// Ordenar socios
+		Socios02.OrdenarSocios(socios);
+
+		// Mostrar socios después de ordenar
+		System.out.println("\n\tSocios después de ordenar:");
+		imprimirSocios(socios);
+	}
+
+	// Método para imprimir los datos de los socios
+	public static void imprimirSocios(Socios02[] socios) {
+		for (Socios02 socio : socios) {
+			System.out.println("ID Socio: " + socio.getId_socio() + ", Nombre: " + socio.getNombre() + ", Cuenta: "
+					+ socio.getCuenta());
+		}
 
 	}
 
