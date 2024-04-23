c650402b00b5e70b3d32ff39e9942f331db1813b
Noelia Baños <102227812+Nissolia@users.noreply.github.com>
2024-04-23 10:00:05 +0200

    Add files via upload

---
+++ JAVA/Interfaces_ej01/BucketSort.java
@@ -0,0 +1,13 @@
+package Interfaces_ej01;
+
+import java.util.Comparator;
+
+public class BucketSort implements Comparator<Socios> {
+
+	@Override
+	public int compare(Socios o1, Socios o2) {
+
+		return 0;
+	}
+
+}
