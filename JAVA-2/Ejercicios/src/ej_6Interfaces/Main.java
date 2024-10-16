18795b3337c28b59437e40c1c4ac727da245fea2
Noelia Baños <102227812+Nissolia@users.noreply.github.com>
2024-10-16 20:21:41 +0200

    Add files via upload

---
+++ JAVA-2/Ejercicios/src/ej_6Interfaces/Main.java
@@ -0,0 +1,11 @@
+package ej_6Interfaces;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Detergente prueba = new Detergente("Ariel",2.3);
+System.out.println(prueba);
+	}
+
+}
